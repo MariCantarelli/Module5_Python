ffded699d767268745e2420303b0db6989e9c8cb
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 18:01:30 -0300

    Manipulating Lists

---
+++ Class2.py
@@ -0,0 +1,18 @@
+#Manipulating Lists 
+
+    #Armazenar mais de uma informação em variaveis 
+    #Manter a sequencia dos dados em uma variavel 
+
+city1 = 'Rio de Janeiro'
+city2 = 'São Paulo'
+city3 = 'Salvador'
+
+cities = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Goiana']
+#          0                 1            2           3
+#valores positivos do index começam da esquerda para a direita
+#valores negativos do index começam da direita para a esquerd
+#numbers = [2, 4, 6]
+
+cities[0] = 'Brasilia'
+print(cities)
+#print(numbers)
