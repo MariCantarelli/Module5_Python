27a3d2338e53fd55ed87c9941066c7d53f4e461c
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 18:21:45 -0300

    Functions inside lists

---
+++ Class3.py
@@ -0,0 +1,16 @@
+#Functions inside lists 
+
+city1 = 'Rio de Janeiro'
+city2 = 'São Paulo'
+city3 = 'Salvador'
+
+cities = ['Rio de Janeiro', 'São Paulo', 'Salvador', 'Goiana']
+#          0                 1            2           3
+
+#cities.append('Santa Catarina') Insere na lista
+#cities.remove('Salvador') Retira da lista
+#cities.insert(1, 'Santa Catarina') Coloca na lista em tal posição
+#cities.pop(0) #Retira o 0 
+cities.sort() #Ordem alfabetica
+print(cities)
+
