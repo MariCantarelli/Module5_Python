21d5a7a6cb37147d22f00886426af5672b82a599
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 17:40:46 -0300

    Lists

---
+++ Class1.py
@@ -0,0 +1,12 @@
+#Lists 
+
+    #Armazenar mais de uma informação em variaveis 
+    #Manter a sequencia dos dados em uma variavel 
+
+city1 = 'Rio de Janeiro'
+city2 = 'São Paulo'
+city3 = 'Salvador'
+
+cities = ['Rio de Janeiro', 'São Paulo', 'Salvador']
+print(cities)
+
