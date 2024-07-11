4580c7c4a673829ec0124c0b24d21de7c1b6bf32
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 18:38:23 -0300

    Concatenating lists

---
+++ Class4.py
@@ -0,0 +1,16 @@
+#Concatenating lists
+
+'''numbers = [2, 3 , 4 , 5]
+letters = ['a', 'b', 'c', 'd']
+
+final = numbers * 4
+final = numbers + letters 
+numbers.extend(letters) é o mesmo do de cima
+print(numbers)'''
+
+numbers = [1, 2, 3, 4]
+print(numbers[2])
+itens = [['item1', 'item2'], ['item3', 'item4']]
+#        0                  1
+#        0         1        0         1
+print(itens[1][1])
