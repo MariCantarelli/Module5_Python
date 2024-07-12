eebf9325efa8869194972f78300540ee5e1a7509
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 22:15:59 -0300

    Creating sets

---
+++ Class12.py
@@ -0,0 +1,17 @@
+#Creating sets 
+
+#Set (listas)
+#   Evita itens duplicados
+#   Não utiliza index
+
+list1 =  [10, 20, 30, 40, 50]
+list2 = [10, 20, 60, 70]
+
+num1 = set(list1)
+num2 = set(list2)
+
+print(num1 | num2) #Union
+print(num1 - num2) #Difference 
+print(num1 ^ num2) #Symmetric difference
+print(num1 & num2) #And
+print(len(num1))
