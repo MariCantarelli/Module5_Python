f8ddab85d505eafdb5b35902e6b386e2e3258865
Marina Cantarelli <macbarroca@gmail.com>
2024-07-11 21:07:42 -0300

    Aggregating two lists with zip

---
+++ Class8.py
@@ -0,0 +1,13 @@
+#Aggregating two lists with zip
+
+colors =['yellow', 'green', 'blue', 'red']
+value = [10, 20, 30, 40]
+
+two_lists = zip(value, colors)
+print(list(two_lists))
+
+
+'''var = list('buy')
+print(var)'''
+
+
