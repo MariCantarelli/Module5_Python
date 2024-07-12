6f8e6b12e71b9b78585b8dce2d8f4a8bb5f3e737
Marina Cantarelli <macbarroca@gmail.com>
2024-07-12 14:01:18 -0300

    Introduction to dictionaries

---
+++ Class15.py
@@ -0,0 +1,8 @@
+#Introduction to dictionaries
+
+#Utiliza index no formato de keys e values 
+#Aceita string, integer, float, boolean...
+
+student = {'name': 'ana', 'age': 16, 'final_grade': 'A', 'approval': True}
+
+print(student['age'])
